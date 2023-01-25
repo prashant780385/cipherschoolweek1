a6eaedde19ce38acd7b1a5b6340930696b54cd59
prashant780385 <122338301+prashant780385@users.noreply.github.com>
2023-01-25 11:21:24 +0530

    Add files via upload

---
+++ LECTURE 4 CIPHER.cpp
@@ -0,0 +1,78 @@
+QUESTION NUMBER -1
+
+
+
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cout<<pow(2,4);
+
+    return 0;
+}
+
+
+
+
+
+
+
+QUESTION NUMBER -2
+
+
+
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int power(int base, int pow) {
+    int num =1, i;
+    for(i=1; i<pow;i++) {
+        num*= base;
+    }
+    return num;
+}
+
+
+
+int main()
+{
+    cout<<pow(2,4);
+
+    return 0;
+}
+
+
+
+
+
+
+QUESTION NUMBER -3
+
+
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int power(int base, int pow) {
+    if (pow ==0) return 1;
+    
+    int smlAns = power(base, pow -1);
+    return smlAns*base;
+}
+    
+    
+
+
+
+int main()
+{
+    cout<<pow(2,0);
+
+    return 0;
+}
